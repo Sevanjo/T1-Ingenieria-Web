9e1e820946088596e85d8d82c1b5116192137a91
Sevanjo <sperezr231@gmail.com>
2025-03-14 10:39:23 -0500

    Ultimos ajustes

---
+++ .history/models/Producto_20250313211502.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Importar conexión a la BD
+
+const Producto = sequelize.define('Producto', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    precio: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    Imagen: { 
+        type: DataTypes.STRING,
+         allowNull: true 
+        }
+});
+
+module.exports = Producto;
